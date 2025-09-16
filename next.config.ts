cdf50844b2010cf1d9127c8223183a48689d0cc7
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-09-16 13:29:44 +0300

    feat(dashboard): add side navbar in dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+
+const path = require('path');
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
